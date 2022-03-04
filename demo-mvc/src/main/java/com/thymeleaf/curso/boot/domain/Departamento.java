e6ced22d87940a3b51193aff9dc91f90e2089d5d
hilberto <betogouveia1@outlook.com>
2022-03-04 17:52:01 -0300

    primeiro commit

---
+++ demo-mvc/src/main/java/com/thymeleaf/curso/boot/domain/Departamento.java
@@ -0,0 +1,38 @@
+package com.thymeleaf.curso.boot.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/*Anotações para mapeamento*/
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "DEPARTAMENTOS")
+public class Departamento extends Enttity<Long> {
+
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	String nome;
+
+	@OneToMany(mappedBy = "departamento")
+	private List<Cargo> cargos;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Cargo> getCargos() {
+		return cargos;
+	}
+
+	public void setCargos(List<Cargo> cargos) {
+		this.cargos = cargos;
+	}
+}
